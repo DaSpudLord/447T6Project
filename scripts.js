3459bd4e48ee95bb2f7e34a7b6a02084c64883f9
DaSpudLord <42848798+DaSpudLord@users.noreply.github.com>
2021-11-09 14:27:20 -0500

    The tabs now work
    The tabs now work

--- scripts.js
+++ scripts.js
@@ -1,3 +1,36 @@
+const TAB_ACTIVE_CLASS = "tab-active";
+
+function tabClickSelect(button, all_tab_containers_class, target_tab_container_id)
+{ return tabClick(button, document.getElementsByClassName(all_tab_containers_class), document.getElementById(target_tab_container_id)); }
+function tabClick(button, all_tab_containers, target_tab_container)
+{
+	if (button.classList.contains(TAB_ACTIVE_CLASS))
+		return;
+
+	for (let i = 0; i < all_tab_containers.length; i++)
+	{
+		if (all_tab_containers[i] === target_tab_container)
+			continue;
+		all_tab_containers[i].hidden = true;
+	}
+
+	let buttons = button.parentElement.children;
+	for (let i = 0; i < buttons.length; i++)
+	{
+		if (buttons[i].tagName != "BUTTON")
+			continue;
+		if (buttons[i] === button)
+			continue;
+		buttons[i].classList.remove(TAB_ACTIVE_CLASS);
+	}
+
+	button.classList.add(TAB_ACTIVE_CLASS);
+	if (target_tab_container !== null)
+		target_tab_container.hidden = false;
+}
+
+
+
 /*
  * Returns an L.tileLayer() of the specified type from Mapbox.
  * This is a convenience method that handles setting all the proper settings for the tiles
